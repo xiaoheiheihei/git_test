bd42d4d69187f4451909a4a2bf4e16fe35e1cff5
xiaohei <729968345@qq.com>
2019-05-31 19:19:08 +0800

    git_test version 5

--- src/main/java/com/baizhi/test/TestGit.java
+++ src/main/java/com/baizhi/test/TestGit.java
@@ -5,5 +5,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("这是一个测试类");
         System.out.println("是的");
+        System.out.println("不是的");
     }
 }
